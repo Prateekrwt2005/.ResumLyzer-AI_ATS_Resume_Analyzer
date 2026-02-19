0b6ec7fd45e1c1aca505731e99475fb5d2a2cd73
Prateek Rawat <prateekrwt09@gmail.com>
2026-02-19 18:05:25 +0530

    ready for render deploy

--- ai-ats-resume-analyzer/server/index.js
+++ ai-ats-resume-analyzer/server/index.js
@@ -73,7 +73,8 @@ mongoose
 // --------------------
 // START SERVER
 // --------------------
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
   console.log(
